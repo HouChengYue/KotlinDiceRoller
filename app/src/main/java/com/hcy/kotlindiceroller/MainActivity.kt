e48c3863c64197e20499239ccee11f3c468dd2c4
侯程月 <437286343@qq.com>
2020-09-17 16:00:44 +0800

    第二节课内容

--- app/src/main/java/com/hcy/kotlindiceroller/MainActivity.kt
+++ app/src/main/java/com/hcy/kotlindiceroller/MainActivity.kt
@@ -15,3 +15,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+fun main(){
+    val myFirstDice = Dice(32)
+    val rollResult = myFirstDice.roll()
+
+    /**
+     * 幸运数字
+     */
+    val luckyNumber=4
+//    打印一句
+//    println("你的${myFirstDice.numSides}面骰子，投中点数是${rollResult}")
+//    if-else
+//    if (rollResult==luckyNumber){
+//        println("你赢了！")
+//    }else{
+//        println("你没有赢，再试一次吧！")
+//    }
+//    if (rollResult == luckyNumber) {
+//        println("You win!")
+//    } else if (rollResult == 1) {
+//        println("So sorry! You rolled a 1. Try again!")
+//    } else if (rollResult == 2) {
+//        println("Sadly, you rolled a 2. Try again!")
+//    } else if (rollResult == 3) {
+//        println("Unfortunately, you rolled a 3. Try again!")
+//    } else if (rollResult == 4) {
+//        println("No luck! You rolled a 4. Try again!")
+//    } else if (rollResult == 5) {
+//        println("Don't cry! You rolled a 5. Try again!")
+//    } else {
+//        println("Apologies! you rolled a 6. Try again!")
+//    }
+//    wihen 的使用
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        4 -> println("No luck! You rolled a 4. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! you rolled a 6. Try again!")
+        else->{println("Apologies! you rolled a ${rollResult}. Try again!")}
+    }
+}
+
+/**
+ * 骰子
+ * @param numSides 变数
+ */
+class Dice(val numSides:Int){
+    /**
+     * 旋转
+     */
+    fun roll():Int{
+        return (1..numSides).random()
+    }
+}
+
