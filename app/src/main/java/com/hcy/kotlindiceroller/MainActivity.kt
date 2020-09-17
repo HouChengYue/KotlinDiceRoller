d02998315b271254af5bdb719fe73422df8c9f85
侯程月 <437286343@qq.com>
2020-09-17 15:44:05 +0800

    创建项目

---
+++ app/src/main/java/com/hcy/kotlindiceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hcy.kotlindiceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Kotlin 学习项目
+ * https://codelabs.developers.google.com/codelabs/basic-android-kotlin-training-conditional-dice-roll-behavior/index.html?index=..%2F..basic-android-kotlin-training#2
+ * @author 侯程月
+ * @date 2020/09/17
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
