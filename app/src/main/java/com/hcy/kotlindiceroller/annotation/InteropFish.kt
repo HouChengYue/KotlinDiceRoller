138e8012291c89b1672ba56491a8f901e06bff6d
侯程月 <437286343@qq.com>
2020-09-19 14:12:09 +0800

    注释的学习

---
+++ app/src/main/java/com/hcy/kotlindiceroller/annotation/InteropFish.kt
@@ -0,0 +1,7 @@
+package com.hcy.kotlindiceroller.annotation
+
+/**
+ * 描述：
+ * @author 侯程月
+ * @date 2020/9/19
+ */
---
+++ app/src/main/java/com/hcy/kotlindiceroller/annotation/test.kt
@@ -0,0 +1,8 @@
+package com.hcy.kotlindiceroller.annotation
+
+/**
+ * 描述：
+ * @author 侯程月
+ * @date 2020/9/19
+ */class test {
+}
